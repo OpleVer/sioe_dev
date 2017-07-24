d02dcf41299ae305c5afec049b74a16412286bbd
Dayane Hernandez <dayao@MacBook-Air-de-Dayane.local>
2017-07-24 12:56:22 -0500

    primer commit

---
+++ src/main/webapp/app/blocks/config/uib-pager.config.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('sioeDevApp')
+        .config(pagerConfig);
+
+    pagerConfig.$inject = ['uibPagerConfig', 'paginationConstants'];
+
+    function pagerConfig(uibPagerConfig, paginationConstants) {
+        uibPagerConfig.itemsPerPage = paginationConstants.itemsPerPage;
+        uibPagerConfig.previousText = '«';
+        uibPagerConfig.nextText = '»';
+    }
+})();
